42970d00ccaa217e7a4b5440258e3b603710a1aa
Nurlan Bolayev <bolayev.nurlan@gmail.com>
2021-01-20 22:57:38 +0400

    Add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::create([
+            'name' => 'United Skills',
+            'email' => 'talent@unitedskills.az',
+            'password' => \Hash::make('talent')
+        ]);
+
+
+        $posts = Post::factory()
+            ->has(Comment::factory()->count(2))
+            ->create();
+
+        $posts->each(fn($post) => $post->comments->each(function ($comment) use ($post) {
+            Comment::factory()
+                ->count(2)
+                ->create([
+                    'post_id' => $post,
+                    'parent_id' => $comment,
+                ]);
+        }));
+
+        Post::factory()
+            ->has(Comment::factory()->count(3))
+            ->create();
+
+
+        Post::factory()
+            ->create();
     }
 }
